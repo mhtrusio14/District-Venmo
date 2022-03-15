6d223a9414115e1cfb8c3d4f3d0af88eb321cdf9
Michael <mhtrusio@hotmail.com>
2022-03-15 04:04:06 -0400

    Extra Tinkering

--- GenerateSecondSheet.py
+++ GenerateSecondSheet.py
@@ -8,4 +8,10 @@ scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/aut
          "https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 gclient = gspread.authorize(creds)
-sheet = gclient.open("District Test").sheet1
+sheet = gclient.open("District Test")
+worksheet1 = sheet.sheet1
+try:
+    worksheet2 = sheet.add_worksheet(title="Calculation Sheet", rows=100, cols = 26)
+except:
+    print("Did not create a duplicate sheet")
+    worksheet2 = sheet.worksheet("Calculation Sheet")
